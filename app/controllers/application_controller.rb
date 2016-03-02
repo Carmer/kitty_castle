bcd06709bfa18995655b14dd789ce6e48b22dbe4
Andrew Carmer <carmerandrew@gmail.com>
2016-03-02 09:41:44 -0700

    add routes and session info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_kitty
+    @current_kitty ||= Kitty.find_by(id: session[:kitty_id]) if session[:kitty_id]
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def create
+    @kitty = Kitty.find(params[:id])
+
+    if @kitty && @kitty.authenticate(params[:session][:password])
+      flash[:notice] = "Succsessfully Logged In As #{@kitty.name}"
+      session[:kitty_id] = @kitty.id
+      redirect_to kitty_path
+    else
+      flash[:error] = "Invalid Credentials. Please Try Again."
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    flash[:notice] = "Successfully logged out meow."
+    redirect_to root_path
+  end
+end
