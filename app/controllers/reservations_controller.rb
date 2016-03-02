ad6f84ed349b1f175935bbeca72d09e9c604eeb8
Andrew Carmer <carmerandrew@gmail.com>
2016-03-02 10:23:13 -0700

    put class_method example into controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -12,6 +12,11 @@ class ReservationsController < ApplicationController
     end
   end
 
+  def index
+    @complete   = Reservation.complete
+    @today      = Reservation.today
+    @newer_than = Reservation.newer_than(params[:date])
+  end
 
   private
 
