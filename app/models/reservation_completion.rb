2efd3d9b809ee9fcc2555b6b4c09a65acc69048d
Andrew Carmer <CarmerAndrew@gmail.com>
2017-04-12 09:45:01 -0600

    Update reservation_completion.rb

--- app/models/reservation_completion.rb
+++ app/models/reservation_completion.rb
@@ -1,15 +1,15 @@
 class ReservationCompletion
 
-  def create(reservation)
+  def self.create(reservation)
      send_reservation_confirmation(reservation)
      set_kitty_to_active(reservation)
   end
 
-  def send_reservation_confirmation(reservation)
+  def self.send_reservation_confirmation(reservation)
     ReservationMailer.reservation_confirmation(reservation.kitty, reservation.castle).deliver
   end
 
-  def set_kitty_to_active(reservation)
+  def self.set_kitty_to_active(reservation)
     reservation.kitty.update_attributes(status: “active”)
   end
 end
